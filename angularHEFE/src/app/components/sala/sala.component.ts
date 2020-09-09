f11babc34babb84773cc73cb79f57406919d03ea
TCxDouglas <douglasalexander683@gmail.com>
2020-09-08 22:54:33 -0600

    Correcion Bug: Crear Salas

--- angularHEFE/src/app/components/sala/sala.component.ts
+++ angularHEFE/src/app/components/sala/sala.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { RoomsService } from '../../services/rooms.service';
 import { Room } from '../../models/room';
 import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
+
 @Component({
   selector: 'app-sala',
   templateUrl: './sala.component.html',
@@ -12,23 +14,39 @@ export class SalaComponent implements OnInit {
   @Input() room: Room;
   private uidCreator: string;
   public numberMembers: number;
+  public user: any;
+  public userData: any;
   constructor(
     public roomServices: RoomsService,
-    public _router: Router
+    public _router: Router,
+    public userService: UserService
   ) {}
 
   async ngOnInit(){
-    await this.roomServices.getCreatorRoom(this.room.id).then((creator) => {
-      this.uidCreator = creator.data().uidCreador;
-    });
+    this.user = await this.userService.getCurrentUser();
+    this.userService.getUserData(this.user.uid).subscribe(data =>{
+      this.userData = data;
 
-    this.roomServices.getCollectionRoomAsync(this.uidCreator, this.room.id).subscribe(collection=>{
+      if(this.userData.cuenta=='Docente'){
+        this.uidCreator = this.user.uid;
+        this.getCollectionRoom();
+      }else{
+        this.roomServices.getCreatorRoom(this.room.id).then((creator) => {
+          //console.log(creator.data());
+          this.uidCreator = creator.data().uidCreador;
+          this.getCollectionRoom();
+        });
+      }
+    }); 
+  }
+
+  private getCollectionRoom() {
+    this.roomServices.getCollectionRoomAsync(this.uidCreator, this.room.id).subscribe(collection => {
       this.numberMembers = collection.length;
     });
   }
 
-  detailsRoom(): void{
-    
+  detailsRoom(): void{  
     this._router.navigate(['detailsRoom',this.uidCreator, this.room.id]);
   }
 
