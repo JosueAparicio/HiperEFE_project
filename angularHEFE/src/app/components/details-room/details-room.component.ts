63cc2790d0745c6f4bdab3057fce884b0e3dc4a3
TCxDouglas <douglasalexander683@gmail.com>
2020-09-15 20:22:08 -0600

    Arreglo de diseño Details Room

--- angularHEFE/src/app/components/details-room/details-room.component.ts
+++ angularHEFE/src/app/components/details-room/details-room.component.ts
@@ -193,10 +193,11 @@ export class DetailsRoomComponent implements OnInit {
 
   }
 
-
   openSnackBar(message: string, action: string) {
     this._snackBar.open(message, action, {
       duration: 5000,
     });
   }
+
+  
 }
