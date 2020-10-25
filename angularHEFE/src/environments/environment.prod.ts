9402e6b14d7af022598733e83ca0c9faf3368cfc
TCxDouglas <douglasalexander683@gmail.com>
2020-10-25 10:18:45 -0600

    Arreglando el proyecto a modo produccion

--- angularHEFE/src/environments/environment.prod.ts
+++ angularHEFE/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCevSdUwA4xYd-BKjm6kR_VjLZdY1PQKvU",
+    authDomain: "virtual-city-eb37f.firebaseapp.com",
+    databaseURL: "https://virtual-city-eb37f.firebaseio.com",
+    projectId: "virtual-city-eb37f",
+    storageBucket: "virtual-city-eb37f.appspot.com",
+    messagingSenderId: "1000304336225",
+    appId: "1:1000304336225:web:052c3f8426e8d98598f920"
+  }
 };
