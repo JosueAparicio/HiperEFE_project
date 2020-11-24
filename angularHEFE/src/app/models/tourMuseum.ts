199d50a32e0be1538668765f39ceea9d96b23751
TCxDouglas <douglasalexander683@gmail.com>
2020-11-23 23:32:50 -0600

    comienzo del Tour HSV

---
+++ angularHEFE/src/app/models/tourMuseum.ts
@@ -0,0 +1,18 @@
+export class tour {
+    private startTour = {X:0, y:0, z:0};
+    private positionTour = [{}, {}, {}]
+    constructor(){
+    }
+
+    public next(){
+
+    }
+
+    public prev(){
+
+    }
+
+    public exit(){
+
+    }
+}
